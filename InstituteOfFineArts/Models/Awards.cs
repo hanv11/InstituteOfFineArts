42d3e9c15939d0487687d8b488e95fb44faba89c
quocanh0106 <anhdqth1804005@fpt.edu.vn>
2020-02-11 18:03:54 +0700

    feature#0.1: create model Awarks

---
+++ InstituteOfFineArts/Models/Awards.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstituteOfFineArts.Models
+{
+    public class Awards
+    {
+        public string SubmissionId { get; set; }
+        public string AwardName { get; set; }
+        public virtual ICollection<Submissions> submissions { get; set; }
+    }
+}
