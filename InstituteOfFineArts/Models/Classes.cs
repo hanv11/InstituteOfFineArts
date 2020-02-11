b97c6f2340c51dd60f22a8bb5d0a369049b28911
quocanh0106 <anhdqth1804005@fpt.edu.vn>
2020-02-11 18:06:03 +0700

    feature#0.1: create model Classes

---
+++ InstituteOfFineArts/Models/Classes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstituteOfFineArts.Models
+{
+    public class Classes
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public virtual ICollection<Classes> classes { get; set; }
+
+    }
+}
