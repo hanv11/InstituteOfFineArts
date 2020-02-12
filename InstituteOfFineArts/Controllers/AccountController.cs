13bbcca0ad3b9a37255ec049e95047b66fd0da7a
hanv11 <hanguyen11mm@gmail.com>
2020-02-12 22:56:38 +0700

    update Account model

--- InstituteOfFineArts/Controllers/AccountController.cs
+++ InstituteOfFineArts/Controllers/AccountController.cs
@@ -367,7 +367,7 @@ namespace InstituteOfFineArts.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new Account() { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
