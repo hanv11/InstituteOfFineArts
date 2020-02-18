c122da68be1cb109d5a133995122dc097c551745
hanv11 <hanguyen11mm@gmail.com>
2020-02-18 22:42:34 +0700

    create new competition by teacher

--- InstituteOfFineArts/Areas/Teacher/Controllers/CompetitionController.cs
+++ InstituteOfFineArts/Areas/Teacher/Controllers/CompetitionController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InstituteOfFineArts.Models;
 
 namespace InstituteOfFineArts.Areas.Teacher.Controllers
 {
     public class CompetitionController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Teacher/Competition
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CompetitionId,CompetitionName,StartDate,EndDate,Image,AwardDetails,Description")] Competition competition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Competitions.Add(competition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(competition);
         }
     }
 }
