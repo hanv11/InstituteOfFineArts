807fe02e6498f3532b8e2b119708f32658858cfe
quocanh0106 <anhdqth1804005@fpt.edu.vn>
2020-02-22 14:28:07 +0700

    add list competition to adminpage

--- InstituteOfFineArts/Areas/Teacher/Controllers/CompetitionController.cs
+++ InstituteOfFineArts/Areas/Teacher/Controllers/CompetitionController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using InstituteOfFineArts.Models;
@@ -13,7 +15,20 @@ namespace InstituteOfFineArts.Areas.Teacher.Controllers
         // GET: Teacher/Competition
         public ActionResult Index()
         {
-            return View();
+            return View(db.Competitions.ToList());
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Competition competition = db.Competitions.Find(id);
+            if (competition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(competition);
         }
 
         public ActionResult Create()
@@ -34,5 +49,61 @@ namespace InstituteOfFineArts.Areas.Teacher.Controllers
 
             return View(competition);
         }
+        [Authorize(Roles = "Admin,Teacher")]
+        // GET: Competitions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Competition competition = db.Competitions.Find(id);
+            if (competition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(competition);
+        }
+
+        // POST: Competitions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Teacher")]
+        public ActionResult Edit([Bind(Include = "CompetitionId,CompetitionName,StartDate,EndDate,Image,AwardDetails,Description")] Competition competition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(competition).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(competition);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Competition competition = db.Competitions.Find(id);
+            if (competition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(competition);
+        }
+        // POST: Competitions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Competition competition = db.Competitions.Find(id);
+            db.Competitions.Remove(competition);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
