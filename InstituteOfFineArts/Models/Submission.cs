dcd999374563ca17475e295141f0e5413684cfa9
kylix.par <minhvcth1806008@fpt.edu.vn>
2020-03-03 13:43:55 +0700

    display point in detail

--- InstituteOfFineArts/Models/Submission.cs
+++ InstituteOfFineArts/Models/Submission.cs
@@ -5,35 +5,44 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using Microsoft.Owin.Security.Provider;
 
 namespace InstituteOfFineArts.Models
 {
     public class Submission
     {
-        [Key]
-        public int SubmissionId { get; set; }
+        [Key] public int SubmissionId { get; set; }
         public int CompetitionId { get; set; }
+
         [Required(ErrorMessage = "Update your submission.")]
         public string Picture { get; set; }
+
         [DisplayName("Name")]
         [Required(ErrorMessage = "Please enter submission name.")]
         public string SubmissionName { get; set; }
+
         [DataType(DataType.DateTime)]
         [DisplayName("Updated Post")]
         public DateTime? UpdatedAt { get; set; }
+
         public DateTime? CreatedAt { get; set; }
+
         // nguoi ta ra submission
-        [ForeignKey("Creator")]
-        public string CreatorId { get; set; }
+        [ForeignKey("Creator")] public string CreatorId { get; set; }
         public virtual Account Creator { get; set; }
+
         [Required(ErrorMessage = "Please enter decription of submission.")]
         public string Description { get; set; }
+
         public virtual Competition Competition { get; set; }
-        public virtual ICollection<Mark > Marks { get; set; }
+        public virtual ICollection<Mark> Marks { get; set; }
+
         public Submission()
         {
         }
+
         public SubmissionStatus Status { get; set; }
+
         public enum SubmissionStatus
         {
             Pending = 0,
@@ -43,20 +52,22 @@ namespace InstituteOfFineArts.Models
 
         public virtual Award Award { get; set; }
 
-        public  double MarkAverage()
+        public double MarkAverage
         {
-            var sumOfMark = 0;
-            if (!Marks.Any())
-            {
-                return 0;
-            }
+            get {
+                var sumOfMark = 0;
+                if (!Marks.Any())
+                {
+                    return 0;
+                }
 
-            foreach (var item in Marks)
-            {
-                sumOfMark += (int) item.Marks * 2;
-            }
+                foreach (var item in Marks)
+                {
+                    sumOfMark += (int)item.Marks;
+                }
 
-            return (double)sumOfMark / Marks.Count;
+                return (double)sumOfMark / Marks.Count;
+            }
 
         }
         public Submission(int competid,string creatorid,string pic, string subname, string descrip, DateTime createdat)
